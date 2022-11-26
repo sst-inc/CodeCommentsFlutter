17729114a7733719f4dfe2ac537f16fa95e0c963
lukeskywalker22 <ekulyeo22@gmail.com>
2022-11-26 16:56:14 +0800

    made settings page

--- codecomments/lib/main.dart
+++ codecomments/lib/main.dart
@@ -105,16 +105,24 @@ class _MyHomePageState extends State<MyHomePage> {
   );
 
   var settingsPage = SafeArea(
-    child: Column(
-      children: [
-        ClipRRect(
-            child: Image.network(
-          "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
-          height: 150,
-        ))
-      ],
+      child: SafeArea(
+    child: Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+              child: Image.network(
+                "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
+                height: 300,
+              ),
+            ),
+          )
+        ],
+      ),
     ),
-  );
+  ));
 
   @override
   Widget build(BuildContext context) {
