5659679f04d42dae147d7887577f4f102ecd48b5
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-11-27 15:41:14 +0800

    Extracted homepage from main.dart

---
+++ codecomments/lib/Home.dart
@@ -0,0 +1,79 @@
+import 'package:codecomments/themes.dart';
+import 'package:flutter/material.dart';
+
+import 'chats.dart';
+import 'settings.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      darkTheme: darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: _selectedIndex == 0
+              ? Text("Home")
+              : (_selectedIndex == 1 ? Text("Chats") : Text("Settings")),
+        ),
+        body: _selectedIndex == 1 ? ChatsPage() : SettingsPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.chat_bubble,
+                color: Colors.grey,
+              ),
+              label: 'Chats',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.grey,
+              ),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
