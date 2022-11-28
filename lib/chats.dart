cb1ab9a583ef7326e4b6a079bfa7d087a7264080
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-11-28 16:22:47 +0800

    successfully merged remote commit into local repo

---
+++ lib/chats.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatelessWidget {
+  const ChatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+    child: Column(
+      children: [
+        ListView.separated(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (BuildContext context, int index) => Padding(
+                padding: EdgeInsets.all(10),
+                child: ListTile(
+                    title: Row(
+                  children: [
+                    Image.network(
+                      "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
+                      height: 60,
+                    ),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    Center(
+                      child: Column(
+                        children: [
+                          Text(
+                            "Wavin Wagpal $index",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const Text(
+                            "last message sent",
+                            style: TextStyle(fontWeight: FontWeight.w400),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ))),
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(
+                  color: Colors.black,
+                ),
+            itemCount: 5)
+      ],
+    ),
+  );;
+  }
+}
