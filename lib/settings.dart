6bc39cdd727fdbb8cf788755e0f87f5e7686e17b
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-12-13 00:09:59 +0800

    Reworked a lot of the UI and added ability to modify text values

--- lib/settings.dart
+++ lib/settings.dart
@@ -2,88 +2,223 @@ import 'package:code_comments_flutter/devmenu.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool enteredEditMode = false;
+
+  String name = "Person Dude";
+  String email = "person@gmail.com";
+  String occupation = "Programmer";
+  String bio = "hi";
+  String taughtLangs = "Py";
+
+  String _tempBio = "";
+  String _tempTaughtLangs = "";
+  bool _modifiedBio = false;
+  bool _modifiedTempLangs = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
+      floatingActionButton: !enteredEditMode
+          ? FloatingActionButton(
+              child: !enteredEditMode ? Icon(Icons.edit) : Icon(Icons.check),
+              onPressed: () {
+                setState(() {
+                  enteredEditMode = !enteredEditMode;
+                });
+              },
+              enableFeedback: true,
+              backgroundColor: Theme.of(context).primaryColor,
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: FloatingActionButton(
+                    mini: true,
+                    onPressed: () {
+                      setState(() {
+                        _tempBio = "";
+                        _tempTaughtLangs = "";
+                        _modifiedBio = false;
+                        _modifiedTempLangs = false;
+                        enteredEditMode = !enteredEditMode;
+                      });
+                    },
+                    enableFeedback: true,
+                    backgroundColor: Theme.of(context).primaryColor,
+                    child: Icon(Icons.cancel_outlined),
+                  ),
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.check),
+                  onPressed: () {
+                    setState(() {
+                      if (_modifiedBio) {
+                        bio = _tempBio;
+                      } else {
+                        bio = bio;
+                      }
+                      if (_modifiedTempLangs) {
+                        taughtLangs = _tempTaughtLangs;
+                      } else {
+                        taughtLangs = taughtLangs;
+                      }
+                      _tempBio = "";
+                      _tempTaughtLangs = "";
+                      _modifiedBio = false;
+                      _modifiedTempLangs = false;
+                      enteredEditMode = !enteredEditMode;
+                    });
+                  },
+                  enableFeedback: true,
+                  backgroundColor: Theme.of(context).primaryColor,
+                )
+              ],
+            ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(150),
-                child: Image.network(
-                  "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
-                  height: 250,
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(150),
+                    child: Image.network(
+                      "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
+                      height: 250,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 25,
-            ),
-            const Text(
-              "Username: (null)",
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text(
-              "Email: (null)",
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text(
-              "Bio: (null)",
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text(
-              "Occupation: (null)",
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text(
-              "Coding Language: (null)",
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Edit details",
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                var navigator = Navigator.of(context);
-                navigator
-                    .push(CupertinoPageRoute(builder: (context) => DevMenu()));
-              },
-              child: Text(
-                "Show dev menu",
-                style: TextStyle(fontSize: 16),
-              ),
-            )
-          ],
+              const Divider(),
+              !enteredEditMode
+                  ? Column(children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Welcome, ${name}",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "${email}",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          bio,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "${occupation} who teaches ${taughtLangs}",
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ])
+                  : Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: TextFormField(
+                            initialValue: taughtLangs,
+                            onChanged: (value) {
+                              _tempTaughtLangs = value;
+                              _modifiedTempLangs = true;
+                            },
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Taught Languages",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: TextFormField(
+                            initialValue: bio,
+                            onChanged: (value) {
+                              _tempBio = value;
+                              _modifiedBio = true;
+                            },
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Bio",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: TextFormField(
+                            enabled: false,
+                            initialValue: name,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Name",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: TextFormField(
+                            enabled: false,
+                            initialValue: email,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Email",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: TextFormField(
+                            enabled: false,
+                            initialValue: occupation,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Occupation",
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                        ),
+                      ],
+                    ),
+              ElevatedButton(
+                onPressed: () {
+                  var navigator = Navigator.of(context);
+                  navigator.push(
+                      CupertinoPageRoute(builder: (context) => DevMenu()));
+                },
+                child: Text(
+                  "Show dev menu",
+                  style: TextStyle(fontSize: 16),
+                ),
+              )
+            ],
+          ),
         ),
       )),
     );
