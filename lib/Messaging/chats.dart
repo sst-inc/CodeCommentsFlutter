69bf8ef1aa831429338b36f80cf5ac54f883dc2b
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-12-12 16:58:00 +0800

    Enhanced Listview, added new chat FAB

--- lib/Messaging/chats.dart
+++ lib/Messaging/chats.dart
@@ -9,63 +9,85 @@ class ChatsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Chats")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(Icons.edit),
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            ListView.separated(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index) => InkWell(
-                      child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: ListTile(
-                          title: Row(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(30),
-                                child: Image.network(
-                                  "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
-                                  height: 60,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 30,
-                              ),
-                              Center(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Wavin Wagpal $index",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    const Text(
-                                      "last message sent",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (BuildContext context, int index) => InkWell(
+                        child: Card(child: chatsDataView(index)),
+                        onTap: () {
+                          var navigator = Navigator.of(context);
+                          navigator.push(CupertinoPageRoute(
+                              builder: (context) => ChatsInterface()));
+                        },
                       ),
-                      onTap: () {
-                        var navigator = Navigator.of(context);
-                        navigator.push(CupertinoPageRoute(
-                            builder: (context) => ChatsInterface()));
-                      },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Container(),
+                  itemCount: 5)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget chatsDataView(int index) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListTile(
+        title: Row(
+          children: [
+            Flexible(
+              flex: 4,
+              fit: FlexFit.tight,
+              child: Padding(
+                padding: EdgeInsets.zero,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(
+                    "https://media-exp1.licdn.com/dms/image/C5603AQHf-tyMIg6VdQ/profile-displayphoto-shrink_800_800/0/1644684573336?e=2147483647&v=beta&t=fBigrt6W2MFOghS9uEY3WaatzuQtmJnr3yY9dSxs4_Y",
+                    height: 50,
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Padding(padding: EdgeInsets.zero, child: Container()),
+            ),
+            Flexible(
+              flex: 20,
+              fit: FlexFit.tight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Wavin Wagpal $index",
+                      style: const TextStyle(fontWeight: FontWeight.bold),
                     ),
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(
-                      color: Colors.black,
-                    ),
-                itemCount: 5)
+                    const Text(
+                      "last message sent",
+                      style: TextStyle(fontWeight: FontWeight.w400),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
-    ;
   }
 }
