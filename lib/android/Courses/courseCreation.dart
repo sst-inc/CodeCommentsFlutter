ea44e687e312d45c6992a8e2eaa2d17d7845175f
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2023-01-23 22:09:53 +0800

    update course creation popup screen

--- lib/android/Courses/courseCreation.dart
+++ lib/android/Courses/courseCreation.dart
@@ -14,32 +14,113 @@ class _CourseCreationState extends State<CourseCreation> {
       child: Padding(
         padding: MediaQuery.of(context).viewInsets,
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter course name",
-                    border: OutlineInputBorder(),
+            padding: EdgeInsets.all(20),
+            child: Center(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.add,
+                    size: 50,
                   ),
-                ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                        child: Text(
+                          "Upload cover image",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 50,
+                          width: 500,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Enter course name",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 50,
+                          width: 500,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Enter course coding language",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 50,
+                          width: 200,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Enter course limit",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: SizedBox(
+                          height: 50,
+                          width: 800,
+                          child: TextField(
+                            minLines: 2,
+                            maxLines: 4,
+                            decoration: InputDecoration(
+                              hintText: "Enter course description",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+                        child: Text(
+                          "Create",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Enter course description",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+            )),
       ),
     );
   }
